e71d6cd478e0a946a47b6c1335b3799ac6a80451
Sato Hiroaki <satohiroaki88@gmail.com>
2019-08-07 14:24:39 +0900

    brewのための更新

--- setup.bash
+++ setup.bash
@@ -10,22 +10,33 @@ export EDITOR=vim
 
 export PATH=${HOME}/.local/bin/:${PATH}
 export PATH=${HOME}/.local/user/bin:${PATH}
+export PATH=${HOME}/.local/python/bin/:${PATH}
+export PATH=${HOME}/tools/povray/bin/:${PATH}
 export PATH=${HOME}/tools/:${PATH}
+export PATH=${HOME}/tools/brew/bin/:${PATH}
+export PATH=${HOME}/tools/brew/sbin/:${PATH}
 
 export CPATH=${HOME}/.local/include/
+export CPATH=${CPATH}:${HOME}/.local/python/include/
+export CPATH=${HOME}/tools/brew/include/:${CPATH}
 
 export LIBRARY_PATH=${HOME}/.local/lib/
 export LIBRARY_PATH=${LIBRARY_PATH}:${HOME}/.local/python/lib/
-export LIBRARY_PATH=${LIBRARY_PATH}:${HOME}/.local/lib64/
+export LIBRARY_PATH=${LIBRARY_PATH}:${HOME}/.local/lib/
+export LIBRARY_PATH=${HOME}/tools/brew/lib/:${LIBRARY_PATH}
 
 export LD_LIBRARY_PATH=${LIBRARY_PATH}:${LD_LIBRARY_PATH}
+
+export PYTHONPATH=${HOME}/.local/python/bin/
 
 export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:${HOME}/.local/lib/pkgconfig/
 
 export MANPATH=${HOME}/.local/share/man/:
 
-#export CMAKE_MODULE_PATH=${HOME}/.local/lib/cmake/protobuf/
+export CMAKE_MODULE_PATH=${HOME}/.local/lib/cmake/protobuf/
 
 #export EX_ROOT_PATH=~/work/2nd/EXonCS.ASR.v2
 
 set -o vi
+
+#fish;exit
